ad6e569511d901defb5897b3146d8f16a8b5aa6a
Kevin <kevin.horng422@gmail.com>
2022-03-07 19:36:29 -0500

    New repo to save current work on 3D CSS proj. Added button functionality to alter 'screen' and cleaned up the main structure.

---
+++ java.js
@@ -0,0 +1,13 @@
+/* Allows buttons to change color on screen, will add complexity later on with 
+JS to play music */
+function original(){
+    document.getElementById('screen').style.backgroundColor = '#e7e7e7';
+}
+
+function pink(){
+    document.getElementById('screen').style.backgroundColor = '#DEC2B4';
+}
+function green(){
+    document.getElementById('screen').style.backgroundColor = '#D5DEB4';
+}
+
